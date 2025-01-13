faeadfe3b4183b12e38fc8f1701080b1734e5a58
HIMANSHU <himanshukr25030@gmail.com>
2025-01-13 20:02:33 +0530

    Create Basic.js

---
+++ JavaScript/Basic.js
@@ -0,0 +1,8 @@
+//for making the constant data ,use
+const acountid= 123456
+//for define variable we use both var and let;
+var num1= 13246
+let num2= 4643
+// Here let is used becoz of some limitation in var;
+// We can also directly define the value of any string ,char, number(it doesnt show error)
+ city="Patna"
